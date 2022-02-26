f4bec8a69ac87e72980175f6f9fe3fb89d99cfbf
AARAVETI THIRUMALESH <athiruma@athiruma.remote.csb>
2022-02-26 23:38:52 +0530

    Added Login.js component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import Login from './componets/accounts/Login';
+
 function App() {
   return (
     <div className="App">
-      Hello my dear future scope aim
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/login" /> } />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/componets/accounts/Login.js
@@ -0,0 +1,27 @@
+const loginBackground = {
+    background:'#beabff', 
+    position:'fixed',
+     height:'100vh', 
+     width:'100%'  ,
+};
+const Login  = () => {
+    return(
+        <div className="container-fluid"  style={loginBackground}>
+            <div className="row ml-4">
+                <h1 className="offset-1">PCrackers</h1>
+            </div>
+            <div className="row m-1">
+                
+                <div className="offset-1 col-5 col-md-5  ">
+                <img src="assets/pc2.png" class="img-fluid bg-light col-12 col-md-12" alt="Placement Image" />
+                </div>
+                <div className="ml-1 col-6 col-md-5  border border-dark rounded-3" style={{background:'#440582'}}>
+                    <div className="">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Login;
